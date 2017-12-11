8b9a99fc94054881accfad76e040d00a4099c38f
Andrej Babic <andrej.babic@psi.ch>
2017-12-11 15:51:02 +0100

    Improve pump communication driver logging

--- shimadzu_pump/schimadzu.py
+++ shimadzu_pump/schimadzu.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from xml.etree import ElementTree
 
@@ -15,8 +17,12 @@ request_data = {
 
 parameters = {"Flow": "Pumps/Pump/Usual/Flow"}
 
+_logger = logging.getLogger(__name__)
+
 
 def extract_element(name, response):
+    _logger.debug("Trying to extract '%s' from response: %s", name, response)
+
     tree = ElementTree.fromstring(response)
     return tree.find(name).text
 
@@ -25,16 +31,25 @@ class ShimadzuCbm20(object):
     def __init__(self, host):
         self.host = host
 
+        _logger.debug("Starting ShimadzuCbm20 driver with exposed parameters: %s", parameters.keys())
+
         self.endpoints = {"login": "http://%s/cgi-bin/Login.cgi" % self.host,
                           "event": "http://%s/cgi-bin/Event.cgi" % self.host,
                           "method": "http://%s/cgi-bin/Method.cgi" % self.host}
 
     def login(self, user="Admin", password="Admin"):
+
+        _logger.info("Trying to log in as '%s'.", user)
+
         login_data = request_data["login"] % (user, password)
 
         response_text = requests.get(self.endpoints["login"], data=login_data, headers=headers).text
 
+        _logger.debug("Response from pump: %s", response_text)
+
         session_id = extract_element("Certification/SessionID", response_text)
+
+        _logger.debug("Received session_id='%s'.", session_id)
 
         if not session_id:
             raise ValueError("You are already logged in. Please logout first.")
@@ -42,24 +57,44 @@ class ShimadzuCbm20(object):
         return session_id
 
     def logout(self):
+
+        _logger.info("Logging out.")
+
         logout_data = request_data["logout"]
         response_text = requests.get(self.endpoints["login"], data=logout_data, headers=headers).text
 
+        _logger.debug("Response from pump: %s", response_text)
+
     def start(self):
+
+        _logger.info("Starting pump.")
+
         start_data = request_data["start"]
         response_text = requests.get(self.endpoints["event"], data=start_data, headers=headers).text
 
+        _logger.debug("Response from pump: %s", response_text)
+
     def stop(self):
+
+        _logger.info("Stopping pump.")
+
         stop_data = request_data["stop"]
         response_text = requests.get(self.endpoints["event"], data=stop_data, headers=headers).text
 
+        _logger.debug("Response from pump: %s", response_text)
+
     def set(self, name, value):
+
+        _logger.debug("Setting parameter '%s'='%s'.", name, value)
+
         if name not in parameters:
             raise ValueError(
                 "Parameter name '%s' not recognized. Available parameters: %s" % (name, list(parameters.keys())))
 
         set_flow_data = request_data["set"] % {"name": name, "value": value}
         response_text = requests.get(self.endpoints["method"], data=set_flow_data, headers=headers).text
+
+        _logger.debug("Response from pump: %s", response_text)
 
         return extract_element(parameters[name], response_text)
 
