e6ddf52034f405441b65a7037348e84101a40529
Andrej Babic <andrej.babic@psi.ch>
2017-12-12 17:56:52 +0100

    New library version
    - Fix run script.
    - Fix Schimadzu -> Shimadzu naming.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='shimadzu_pump',
-    version="0.0.2",
+    version="0.0.3",
     description="Just a Shimadzu pump controller.",
     author='Paul Scherrer Institute',
     author_email='andrej.babic@psi.ch',
