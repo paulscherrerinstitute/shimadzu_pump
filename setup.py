8c08d0a0abfe63e9e0b4b208eda450a454e65b36
Andrej Babic <andrej.babic@psi.ch>
2018-07-18 15:49:04 +0200

    New library version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='shimadzu_pump',
-    version="1.1.3",
+    version="1.1.4",
     description="Just a Shimadzu pump controller.",
     author='Paul Scherrer Institute',
     author_email='scott.stubbs@psi.ch',
