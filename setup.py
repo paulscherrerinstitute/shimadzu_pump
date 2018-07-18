0f2086c3cc446633aa63ea82afe0816ee3f1d251
Scott Stubbs <sastubbs@gmail.com>
2018-07-18 16:40:29 +0200

    Updated to 1.1.5 for deployment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='shimadzu_pump',
-    version="1.1.4",
+    version="1.1.5",
     description="Just a Shimadzu pump controller.",
     author='Paul Scherrer Institute',
     author_email='scott.stubbs@psi.ch',
