3d8e16f8530e7ac6c9d6e45aa6f0f0b89988b111
Scott A. Stubbs <scott.stubbs@psi.ch>
2018-07-24 13:38:28 +0200

    Bumping to 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='shimadzu_pump',
-    version="1.1.9",
+    version="1.2.0",
     description="Just a Shimadzu pump controller.",
     author='Paul Scherrer Institute',
     author_email='scott.stubbs@psi.ch',
